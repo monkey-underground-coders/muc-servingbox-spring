7745d40b878882f7d2b98c0f731527d461713494
6rayWa1cher <info@a6raywa1cher.com>
2020-11-06 00:54:32 +0300

    springdoc improvements

--- src/main/java/com/a6raywa1cher/mucservingboxspring/config/SpringDocConfig.java
+++ src/main/java/com/a6raywa1cher/mucservingboxspring/config/SpringDocConfig.java
@@ -2,15 +2,22 @@ package com.a6raywa1cher.mucservingboxspring.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+	@Value("${app.version}")
+	private String version;
+
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
+			.info(new Info().title("MUC ServingBox").version(version).license(new License().name("MIT License").url("https://github.com/monkey-underground-coders/muc-servingbox-spring/blob/master/LICENSE")))
 			.components(new Components()
 				.addSecuritySchemes("basic",
 					new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"))
